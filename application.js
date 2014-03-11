7bae2ff6a0e65f12b06272f8c2beca86cb071660
captainbyliner <mcarthurdonald@gmail.com>
2014-03-10 23:25:41 -0400

    updated my css hover effects, added hole numbers in js

--- application.js
+++ application.js
@@ -1,29 +1,34 @@
 
 
 var questions = [{
-	text: 'How many majors did Ben Hogan win?',
+	text: 'How many majors did Jack Nicklaus win?',
 	answers: ['18 Majors', '14 Majors', '19 Majors', '16 Majors'],
 	style: {backgroundColor:"green"},
+	holeNumber: 'Hole #1: A short dog leg to the right, Par 4',
 	correctAnswer: 0
 },{
-	text: 'What is the highest recorded Par 4 score in a PGA Tour event?',
+	text: 'The highest Par 4 score in a PGA Tour event?',
 	answers: ['12', '16', '15', '18'],
 	style: {backgroundColor:"magenta"},
+	holeNumber: 'Hole #2: A short, well-bunkered Par 3',
 	correctAnswer:1
 },{
 	text: 'Who has won the most British Open titles?',
 	answers: ['Tiger Woods', 'Harry Vardon', 'Tom Watson', 'Old Tom Morris'],
 	style: {backgroundColor:"orange"},
+	holeNumber: 'Hole #3: A Par 4, reminiscent of the Road Hole',
 	correctAnswer:1
 },{
 	text: 'Who said "The only thing a golfer needs is more sunlight?"',
 	answers: ['Raymond Floyd', 'Tom Watson', 'Bagger Vance', 'Ben Hogan'],
 	style: {backgroundColor:"purple"},
+	holeNumber: 'Hole #4: A straightaway Par 5 flanked by fescue',
 	correctAnswer:3
 },{
 	text: 'What is the maximum number of clubs a golfer can carry?',
 	answers:['12', '15', '14', '16'],
 	style:{backgroundColor:"yellow"},
+	holeNumber: 'Hole #5: A reachable Par 5 if you have no fear',
 	correctAnswer:2
 }];
 
@@ -38,6 +43,8 @@ function displayQuestion(key) {
 	$(".panel").css(question.style);
 
 	$("#questionDeck").text(question.text);
+
+	$("#questionWrapper").text(question.holeNumber);
 
 	$("#answers").empty();
 
